6c0e63b1415d56e81407c24ea18976ee1fb97468
amphyxs <amphyxs@gmail.com>
2023-08-12 22:14:59 +0300

    :gear: [22] Функциональность навбара

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.scss';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import Projects from './pages/Projects';
 import Blog from './pages/Blog';
@@ -13,32 +13,32 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main/>,
-        errorElement: <NotFound/>,
-    },
-    {
-        path: '/projects',
-        element: <Projects/>,
-        errorElement: <NotFound/>,
-    },
-    {
-        path: '/blog',
-        element: <Blog/>,
-        errorElement: <NotFound/>,
-    },
-    {
-        path: '/books',
-        element: <Books/>,
-        errorElement: <NotFound/>,
-    },
-]);
-
 root.render(
     <React.StrictMode>
-        <Navbar/>
-        <RouterProvider router={router}></RouterProvider>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route
+                    path='/'
+                    element={<Main/>}
+                />
+                <Route
+                    path='/projects'
+                    element={<Projects/>}
+                />
+                <Route
+                    path='/blog'
+                    element={<Blog/>}
+                />
+                <Route
+                    path='/books'
+                    element={<Books/>}
+                />
+                <Route
+                    path='*'
+                    element={<NotFound/>}
+                />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
