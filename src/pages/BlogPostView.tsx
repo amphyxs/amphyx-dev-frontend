3d6789a0ef6aa932b80a9a0fbb3908c42856bf36
amphyxs <amphyxs@gmail.com>
2023-08-24 21:33:27 +0300

    :package: [38] Установка remark-gfm для отображения таблиц

--- src/pages/BlogPostView.tsx
+++ src/pages/BlogPostView.tsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "react-router-dom";
 import ReactMarkdown from 'react-markdown'
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark, atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import remarkGfm from 'remark-gfm'
 
 import Container from "../components/Container"
 import { FullPost } from "../services/Blog";
@@ -24,6 +25,7 @@ const BlogPostView = () => {
                 </div>
                 <div className="dark:text-slate-200">
                     <ReactMarkdown
+                        remarkPlugins={[remarkGfm]}
                         components={{
                             code({ inline, className, children, ...props }) {
                                 const match = /language-(\w+)/.exec(className || '')
