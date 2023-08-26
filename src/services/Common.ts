fcaf63ca07a165a19d475c9c535c1b293e90ecee
amphyxs <amphyxs@gmail.com>
2023-08-26 18:58:15 +0300

    Использование CSS-свойства color-scheme

--- src/services/Common.ts
+++ src/services/Common.ts
@@ -8,9 +8,23 @@ export enum Theme {
 
 export const getLocalTheme = () => {
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
+        setThemeStyles(Theme.Dark)
         return Theme.Dark;
     } else {
+        setThemeStyles(Theme.Light)
         return Theme.Light;
     }
 }
+
+export const setThemeStyles = (theme: Theme) => {
+    const root = document.documentElement;
+    if (theme === Theme.Dark) {
+        root.classList.add('dark');
+        root.style.setProperty('color-scheme', 'dark');
+    }
+    else {
+        root.classList.remove('dark');
+        root.style.setProperty('color-scheme', 'light');
+    }
+}
+
